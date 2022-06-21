ebd15b0a370099dd8e797ca0cedaa2f8d5ce95f0
mahammadalirk <mahammadalirk@code.edu.az>
2022-06-21 16:20:35 +0400

    Visual Studio Project

---
+++ string--reverse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace string__reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseName();
+        }
+       static void ReverseName()
+        {  string name;
+            Console.Write("ReverseName:");
+            {
+                name = Console.ReadLine();
+                for (int i = 0; i < name.Length; i++)
+
+                {
+                    Console.Write(name [name.Length-i-1]);
+
+
+                }
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
